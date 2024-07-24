c24fafd28d1fece1348b73f81aa6aaabaac1663a
Miguel <delgdevoleper@gmail.com>
2024-07-24 20:22:23 -0300

    Criando modal factory (parte 1)

---
+++ src/components/ModalFactory/ModalFactoryIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  <teleport to="body">
+    <div
+      class="fixed top-0 left-0 z-50 flex items-center justify-content w-full h-full bg-black bg-opacity-50"
+    >
+      <div class="fixed mx-10">
+        <div
+          class="felx flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster"
+        ></div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {};
+</script>
