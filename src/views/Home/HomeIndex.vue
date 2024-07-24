0115bb7c38c7df1fe4d1f185478a594b8c4ce576
Miguel <delgdevoleper@gmail.com>
2024-07-24 19:30:48 -0300

    Criando tela inicial(parte 1 e 2)

--- src/views/Home/HomeIndex.vue
+++ src/views/Home/HomeIndex.vue
@@ -1,9 +1,38 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a home</h1>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker 2024</p>
+  </div>
 </template>
 
 <script>
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+import Contact from "./Contact.vue";
+import CustomHeader from "./CustomHeader.vue";
+
 export default {
   name: "HomeIndex",
+  components: { CustomHeader, Contact },
+  setup() {
+    const router = useRouter;
+
+    onMounted(() => {
+      const token = window.localStorage.getItem("token");
+      if (token) {
+        router.push({ name: "Feedbacks" });
+      }
+    });
+
+    function handleLogin() {}
+
+    function handleAccountCreate() {}
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+    };
+  },
 };
 </script>
