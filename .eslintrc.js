69b5e7c8e3570927c3f475c1341c663c15af4b6c
Miguel <delgdevoleper@gmail.com>
2024-07-30 20:34:52 -0300

    criando modal de login pt1

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": "off", // Desabilita a verificação de nomes de componentes multi-palavra
+    "prettier/prettier": ["error", { endOfLine: "lf" }],
   },
   overrides: [
     {
